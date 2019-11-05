0e12d7d1fc57ae9479a0f42430bcf98a1b5cd025
Bob Stone <bob.stone@snapdocs.com>
2019-11-05 09:12:59 -0700

    Got a new job to save

--- jobnotes/app/controllers/jobs_controller.rb
+++ jobnotes/app/controllers/jobs_controller.rb
@@ -3,12 +3,27 @@ class JobsController < ApplicationController
 
     end
 
+    def show
+        @job = Job.find(params[:id])
+    end
+
     def new
 
     end
 
     def create
         #render plain: params[:job].inspect
-        
+        @job = Job.new(job_params)
+        @job.created_at
+        @job.save
+        redirect_to @job
     end
+
+
+    private
+    
+    def job_params
+        params.require(:job).permit!
+    end
+
 end
--- jobnotes/db/migrate/20191104171129_create_jobs.rb
+++ jobnotes/db/migrate/20191104171129_create_jobs.rb
@@ -1,43 +1,17 @@
 class CreateJobs < ActiveRecord::Migration[5.0]
   def change
     create_table :jobs do |t|
-      t.int :id
-      t.string, :customer_ref_id
-      t.string :limit
-      t.string :100
-      t.string, :customer_invoice_id
-      t.string :limit
-      t.string :100
-      t.string :customer_name
-      t.string :string,
-      t.string :limit
-      t.string :100
-      t.string :address
-      t.string :string,
-      t.string :limit
-      t.string :100
-      t.string :city
-      t.string :string,
-      t.string :limit
-      t.string :100
-      t.string :state
-      t.string :string,
-      t.string :limit
-      t.string :30
-      t.string :zip
-      t.string :string,
-      t.string :limit
-      t.string :12
-      t.string :subdivision
-      t.string :string,
-      t.50, :limit
-      t.string :type
-      t.string :string
-      t.string :30,
-      t.string :is_open
-      t.string :boolean
-
-      t.timestamps
+      t.string :customer_ref_id, limit: 100, null: false
+      t.string :customer_invoice_id, limit: 100
+      t.string :customer_name, limit: 100, null: false
+      t.string :address, limit: 100
+      t.string :city, limit: 100
+      t.string :state, limit: 30 
+      t.string :zip, limit: 12
+      t.string :subdivision, limit: 50
+      t.string :job_type, limit: 50
+      t.boolean :is_open, default: true
+      t.timestamps null: false
     end
   end
 end
